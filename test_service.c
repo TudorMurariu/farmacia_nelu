ff36407e05839e354287fe8ce8a397ac7494b78c
Tudor <30391543+TudorMurariu@users.noreply.github.com>
2022-03-24 03:37:38 +0200

    نشط: يدخل Jax في Evasion ، وهو موقف دفاعي ، لمدة ثانيتين ، مما يتسبب في تفادي جميع الهجمات الأساسية غير البرجية ضده طوال المدة. يحصل Jax أيضًا على تقليل الضرر بنسبة 25٪ ، مما يقلل الضرر من جميع قدرات منطقة التأثير التي يتم الحصول عليها من أبطال البطل. يمكن إعادة صياغة Counter Strike بعد ثانية واحدة ، ويتم ذلك تلقائيًا بعد انتهاء المدة.

--- test_service.c
+++ test_service.c
@@ -1,105 +1,114 @@
-#include "service.h"
+#include "test_service.h"
 #include <assert.h>
 #include <stdio.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 
-void test_createElem(){
+
+void test_createElem() {
 	stoc medicament;
-	strcpy(medicament.med.name, "Bixtonim");
+	strcpy_s(medicament.med.name, 20 , "Bixtonim");
 	medicament.med.concentr = 20.01;
 	medicament.quantity = 1;
 
-	char name[] = "Bixtonim"; 
-	stoc compare = createElem(name, 20.01); 
+	char name[] = "Bixtonim";
+	stoc compare = createElem(name, 20.01);
 	medicament.uniqueCode = compare.uniqueCode;
 
-	assert(compareStocs(&medicament,&compare)==1);
+	assert(compareStocs(&medicament, &compare) == 1);
 
 }
 
-void test_addElem(){
+void test_addElem() {
+	Lista* l = init_Lista();
 	vectorDinamic* pharmacy = creeazaVectDin(2);
 
-	addElem(pharmacy, "Bixtonim",20.01);
+	addElem(pharmacy, "Bixtonim", 20.01,l);
 
-	assert(dim(pharmacy)== 1);
-	stoc test =  createElem("Bixtonim",20.01);
-	assert(compareStocs(element(pharmacy, 0),&test)==1);
+	assert(dim(pharmacy) == 1);
+	stoc test = createElem("Bixtonim", 20.01);
+	assert(compareStocs(element(pharmacy, 0), &test) == 1);
 
-	addElem(pharmacy, "Bixtonim", 20.01);
-	assert(element(pharmacy, 0)->quantity == 2 );
+	addElem(pharmacy, "Bixtonim", 20.01,l);
+	assert(element(pharmacy, 0)->quantity == 2);
 
 	distruge(pharmacy);
+	distruge_Lista(l);
 }
 
-void test_searchElem(){
+void test_searchElem() {
+	Lista* l = init_Lista();
 	vectorDinamic* pharmacy = creeazaVectDin(2);
-	addElem(pharmacy, "Bixtonim",20.01);
-	addElem(pharmacy, "Paracetamol",18.2);
-	addElem(pharmacy, "Aspacartim",50);
+	addElem(pharmacy, "Bixtonim", 20.01,l);
+	addElem(pharmacy, "Paracetamol", 18.2,l);
+	addElem(pharmacy, "Aspacartim", 50,l);
 
 	char name[] = "Aspacartim";
 
-	assert(searchElem(pharmacy,name) == 2);
+	assert(searchElem(pharmacy, name) == 2);
 
 	distruge(pharmacy);
-
+	distruge_Lista(l);
 }
 
-void test_modifElem(){
+void test_modifElem() {
+	Lista* l = init_Lista();
 	vectorDinamic* pharmacy = creeazaVectDin(2);
-	
-	addElem(pharmacy, "Bixtonim",20.01);
-	addElem(pharmacy, "Paracetamol",18.2);
-	addElem(pharmacy, "Aspacartim",50);
 
-	modifElem(pharmacy, "Aspacartim", "Theraflu", 0);
+	addElem(pharmacy, "Bixtonim", 20.01,l);
+	addElem(pharmacy, "Paracetamol", 18.2,l);
+	addElem(pharmacy, "Aspacartim", 50,l);
 
-	assert(strcmp(element(pharmacy, 2)->med.name, "Theraflu")==0);
+	modifElem(pharmacy, "Aspacartim", "Theraflu", 0,l);
 
-	modifElem(pharmacy, "Paracetamol", "", 25.07);
+	assert(strcmp(element(pharmacy, 2)->med.name, "Theraflu") == 0);
 
-	assert(floor(element(pharmacy, 1)->med.concentr*100) == 2507);
+	modifElem(pharmacy, "Paracetamol", "", 25.07,l);
+
+	assert(floor(element(pharmacy, 1)->med.concentr * 100) == 2507);
 
 	distruge(pharmacy);
+	distruge_Lista(l);
 }
 
-void test_stergeStoc(){
+void test_stergeStoc() {
+	Lista* l = init_Lista();
 	vectorDinamic* pharma = creeazaVectDin(2);
 
-	addElem(pharma, "Theraflu", 21.5);
-	addElem(pharma, "Theraflu", 21.5);
-	addElem(pharma, "Theraflu", 21.5);
-	addElem(pharma, "Theraflu", 21.5);
+	addElem(pharma, "Theraflu", 21.5,l);
+	addElem(pharma, "Theraflu", 21.5,l);
+	addElem(pharma, "Theraflu", 21.5,l);
+	addElem(pharma, "Theraflu", 21.5,l);
 
-	stergeStoc_s(pharma, "Theraflu");
+	stergeStoc_s(pharma, "Theraflu",l);
 	assert(dim(pharma) == 0);
 
 	distruge(pharma);
+	distruge_Lista(l);
 }
 
-void test_ordonareProduse(){
+void test_ordonareProduse() {
+	Lista* l = init_Lista();
 	vectorDinamic* pharma = creeazaVectDin(2);
 
-	addElem(pharma, "Theraflu", 19.5);
-	addElem(pharma, "Paracetamol", 44);
-	addElem(pharma, "Paracetamol", 44);
-	addElem(pharma, "Paracetamol", 44);
+	addElem(pharma, "Theraflu", 19.5,l);
+	addElem(pharma, "Paracetamol", 44,l);
+	addElem(pharma, "Paracetamol", 44,l);
+	addElem(pharma, "Paracetamol", 44,l);
 
-	addElem(pharma, "Gripovit", 65);
-	addElem(pharma, "Gripovit", 65);
-	addElem(pharma, "Abrolen", 21.5);
-	addElem(pharma, "Abrolen", 21.5);
-	addElem(pharma, "Abrolen", 21.5);
+	addElem(pharma, "Gripovit", 65,l);
+	addElem(pharma, "Gripovit", 65,l);
+	addElem(pharma, "Abrolen", 21.5,l);
+	addElem(pharma, "Abrolen", 21.5,l);
+	addElem(pharma, "Abrolen", 21.5,l);
 
 
 	vectorDinamic* aux = ordonareProduse(pharma, "nume");
 	assert(strcmp(element(aux, 0)->med.name, "Abrolen") == 0);
 	//printf("%s", element(aux, 0)->med.name);
 	distruge(aux);
-	
+
 	aux = ordonareProduse(pharma, "concentratie");
 	assert(strcmp(element(aux, 0)->med.name, "Theraflu") == 0);
 	distruge(aux);
@@ -111,34 +120,36 @@ void test_ordonareProduse(){
 
 
 	distruge(pharma);
+	distruge_Lista(l);
 }
 
-void test_filtrareStoc(){
+void test_filtrareStoc() {
 	vectorDinamic* pharma = creeazaVectDin(2);
+	Lista* l = init_Lista();
+	addElem(pharma, "Theraflu", 19.5, l);
+	addElem(pharma, "Paracetamol", 44, l);
+	addElem(pharma, "Paracetamol", 44, l);
+	addElem(pharma, "Paracetamol", 44, l);
 
-	addElem(pharma, "Theraflu", 19.5);
-	addElem(pharma, "Paracetamol", 44);
-	addElem(pharma, "Paracetamol", 44);
-	addElem(pharma, "Paracetamol", 44);
-
-	addElem(pharma, "Gripovit", 65);
-	addElem(pharma, "Gripovit", 65);
-	addElem(pharma, "Abrolen", 21.5);
-	addElem(pharma, "Abrolen", 21.5);
-	addElem(pharma, "Abrolen", 21.5);
+	addElem(pharma, "Gripovit", 65, l);
+	addElem(pharma, "Gripovit", 65, l);
+	addElem(pharma, "Abrolen", 21.5, l);
+	addElem(pharma, "Abrolen", 21.5, l);
+	addElem(pharma, "Abrolen", 21.5, l);
 
 	vectorDinamic* aux = filtrareStoc(pharma, 2);
 	assert(dim(aux) == 3);
 	distruge(aux);
 
-	aux = filtrareNume(pharma, 'A');
+	aux = filtrareNume(pharma, "A");
 	assert(dim(aux) == 1);
-	
+
 	distruge(aux);
 	distruge(pharma);
+	distruge_Lista(l);
 }
 
-void run_tests(){
+void run_tests() {
 	test_createElem();
 	test_addElem();
 	test_searchElem();
@@ -146,10 +157,4 @@ void run_tests(){
 	test_stergeStoc();
 	test_ordonareProduse();
 	test_filtrareStoc();
-}
-
-int main(){
-	run_tests();
-
-	return 0;
 }
