53a5c10de91c9c2e6748c48a71a212d47b12e3d9
Tudor <30391543+TudorMurariu@users.noreply.github.com>
2022-03-24 03:37:09 +0200

    نشط: يدخل Jax في Evasion ، وهو موقف دفاعي ، لمدة ثانيتين ، مما يتسبب في تفادي جميع الهجمات الأساسية غير البرجية ضده طوال المدة. يحصل Jax أيضًا على تقليل الضرر بنسبة 25٪ ، مما يقلل الضرر من جميع قدرات منطقة التأثير التي يتم الحصول عليها من أبطال البطل. يمكن إعادة صياغة Counter Strike بعد ثانية واحدة ، ويتم ذلك تلقائيًا بعد انتهاء المدة.

--- service.h
+++ service.h
@@ -1,6 +1,9 @@
-#include "repository.h"
+#pragma once
 
-int cmpNume(const void *a, const void *b);
+#include "repository.h"
+#include "VoidList.h"
+
+int cmpNume(const void* a, const void* b);
 
 int cmpConcentr(const void* a, const void* b);
 
@@ -8,22 +11,24 @@ int cmpMarimeStoc(const void* a, const void* b);
 
 void bubble_sort(vectorDinamic* v, int (*comparator)(const void* a, const void* b));
 
-stoc createElem(char *name, float conc);
+stoc createElem(char* name, float conc);
 
 int searchElem(vectorDinamic* pharma, char* name);
 
-int addElem(vectorDinamic* pharma, char* name, float conc);
+int addElem(vectorDinamic* pharma, char* name, float conc, Lista* l);
 
-void actualizare_NumeMedicament(vectorDinamic* pharma, char* name, int pos );
+void actualizare_NumeMedicament(vectorDinamic* pharma, char* name, int pos);
 
-void actualizare_ConcMedicament(vectorDinamic* pharma, float conc,int pos );
+void actualizare_ConcMedicament(vectorDinamic* pharma, float conc, int pos);
 
-int modifElem(vectorDinamic *pharma, char* name, char* modif_name, float modif_conc);
+int modifElem(vectorDinamic* pharma, char* name, char* modif_name, float modif_conc, Lista* l);
 
-void stergeStoc_s(vectorDinamic *pharma, char* name);
+void stergeStoc_s(vectorDinamic* pharma, char* name, Lista* l);
 
 vectorDinamic* ordonareProduse(vectorDinamic* pharma, char* cond);
 
 vectorDinamic* filtrareStoc(vectorDinamic* pharma, int stocMin);
 
 vectorDinamic* filtrareNume(vectorDinamic* pharma, char* condNume);
+
+vectorDinamic* filtrareConcentratie(vectorDinamic* pharma, float concentratie);
