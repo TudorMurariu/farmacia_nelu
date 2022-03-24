5b23155902a79cb32aecbb252c49bb6bc9b22295
Tudor <30391543+TudorMurariu@users.noreply.github.com>
2022-03-24 03:32:41 +0200

    AJUTOR

--- entities.c
+++ entities.c
@@ -1,17 +1,17 @@
 #include "entities.h"
-int compareStocs(stoc* prod1, stoc* prod2){
+int compareStocs(stoc* prod1, stoc* prod2) {
 
-    if(prod1->med.concentr != prod2->med.concentr)
+    if (prod1->med.concentr != prod2->med.concentr)
         return 0;
-    if(strcmp(prod1->med.name, prod2->med.name) != 0)
+    if (strcmp(prod1->med.name, prod2->med.name) != 0)
         return 0;
-    if(prod1->quantity != prod2->quantity)
+    if (prod1->quantity != prod2->quantity)
         return 0;
-    
-    
+
+
     return 1;
 
- 
+
 }
 
 
